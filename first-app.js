c293d25efe24ba9a15ec514ae57b4f225f1a32d8
KunjYadav <kunj.yadav555@gmail.com>
2022-06-10 22:59:46 +0530

    array.map

--- first-app.js
+++ first-app.js
@@ -1,9 +1,16 @@
-const student = {
+const person = {
     name: 'Max',
     age: 29,
     greet() {
-        console.log('Hi, I am ' + this.name);
+      console.log('Hi, I am ' + this.name);
     }
-};
-
-student.greet();
+  };
+  
+  const fruits = ['apple', 'oranges' , '', 'mango', '' , 'lemon'];
+//   for (let fruit of fruits) {
+//       console.log(fruit);
+//   }
+  console.log(fruits.map(fruit => 'Fruit: ' + fruit));
+  console.log(fruits);
+  
+  
