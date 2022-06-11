1accda7aa14519b054a36a02c88e73f98b1c38b6
KunjYadav <kunj.yadav555@gmail.com>
2022-06-11 23:48:09 +0530

    basics

--- first-app.js
+++ first-app.js
@@ -1,16 +1,8 @@
-const person = {
-    name: 'Max',
-    age: 29,
-    greet() {
-      console.log('Hi, I am ' + this.name);
-    }
-  };
-  
-  const fruits = ['apple', 'oranges' , '', 'mango', '' , 'lemon'];
-//   for (let fruit of fruits) {
-//       console.log(fruit);
-//   }
-  console.log(fruits.map(fruit => 'Fruit: ' + fruit));
-  console.log(fruits);
-  
-  
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+    console.log('Kunj Yadav');
+});
+
+server.listen(4000);
