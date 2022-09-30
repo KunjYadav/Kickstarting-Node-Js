d1cc86301a0a7f378ce1e7c9da5b0de630154659
KunjYadav <kunj.yadav555@gmail.com>
2022-09-30 21:58:32 +0530

    Task -2.3 -> difference b/w arrow function & this

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const student = {
-    name: 'Kunj',
-    age: 26,
-    greet() {
-        console.log('Hi, I am ' + this.name);
+class Student {
+    constructor(name, age, marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
     }
-};
+    setPacementAge(minPlacementAge) {
+        return (minMarks) => {
+            if(this.marks > minMarks && this.age > minPlacementAge) {
+                console.log(this.name + " is ready for Placements");
+            } else {
+                console.log(this.name + " is not ready for Placements");
+            }
+        }
+    }
+}
 
-student.greet();
+const Yash = new Student('yash', 25, 75);
+const Vaibhav = new Student('vaibhav', 13, 35);
+
+Yash.setPacementAge(18)(40);
